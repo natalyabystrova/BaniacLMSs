a8743b08c53b44b40938f34f01db4415f2d37c6e
Natalia <natalya.bystrova.93@mail.ru>
2022-11-22 22:11:13 +0300

    init

--- BraniacLMS/config/urls.py
+++ BraniacLMS/config/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-
-from mainapp import views
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.hello_world),
-    path("<str:word>/", views.check_kwargs),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path("mainapp/", include("mainapp.urls")),
 ]
